20f225d60b5e1414c836fa292428feaeab928dd0
sam <sam_morseai@bitbucket.org>
2020-03-23 13:52:19 +0000

    Update Far

--- src/modules/photosynthesis/DO3SE_Photosynthesis_far_ml.F90
+++ src/modules/photosynthesis/DO3SE_Photosynthesis_far_ml.F90
@@ -4,7 +4,8 @@ module DO3SE_Photosynthesis_far_ml
     !     use DO3SE_Photosynthesis_mocks
     ! #else
         use DO3SE_Photosynthesis_mocks
-        use DO3SE_Photosynthesis_helpers_ml
+        use DO3SE_Photosynthesis_helpers_ml, only: calc_fo3_h, calc_f_ls, calc_f_la,  &
+          temp_dep_inhibit, temp_dep
         use DO3SE_PhysicalConstants_ml, only: T0, R, Rn_MJ_to_W, DRATIO
         use DO3SE_ModelConstants_ml, only: UNDEF, LEAF_G_H2O
         ! use DO3SE_Met_ml, only: sunlit_LAI
@@ -17,8 +18,175 @@ module DO3SE_Photosynthesis_far_ml
 
     ! public :: farquhar_photosynthesis
     public :: farquhar_photosynthesis_2
+    public :: get_co2_concentration_in_stomata
 
-    contains
+contains
+
+  pure subroutine get_co2_concentration_in_stomata(c_i_start, c_a, e_a, g_bl, g_sto_0, m, D_0, O3, &
+                                                    dd, O3up_acc_out, & !<< Inputs
+                                                    fO3_h_1_hist, fO3_d_hist, & !<< Inouts
+                                                    g_sto, A_n, A_c, A_j, A_p, R_d, O3up, fO3_d, fO3_h)  ! << Oututs
+
+  real, intent(in) :: c_i_start
+
+  real, intent(in) :: c_a           !< CO2 concentration (ppm)
+  real, intent(in) :: e_a           !< Ambient vapour pressure (Pa)
+  real, intent(in) :: g_bl          !< Boundary layer conductance to H2O vapour (micromol m-2 PLA s-1)
+  real, intent(in) :: g_sto_0       !< Closed stomata conductance (umol/m^2/s)
+  real, intent(in) :: m             !< Species-specific sensitivity to An (dimensionless)
+  real, intent(in) :: D_0           !< "The VPD at which g_sto is reduced by a factor of 2" (Pa) (Leuning et al. 1998)
+  real, intent(in) :: O3            !< ??
+  real, intent(in) :: O3up_acc_out
+  integer, intent(in) :: dd         !< Day number
+
+  real, dimension(366), intent(in) :: fO3_h_1_hist
+  real, dimension(366), intent(in) :: fO3_d_hist
+
+  real, intent(out) :: g_sto                           !two sided stomatal conduct.,vapour [micro mol/(m^2s)]
+  real, intent(out) :: A_n                             !netto assimilation rate            [micro mol/(m^2*s)]
+  real, intent(out) :: A_c                             !Rub. activity. lim. ass. rate      [micro mol/(m^2*s)]
+  real, intent(out) :: A_j                             !electr. transp. lim. ass. rate     [micro mol/(m^2*s)]
+  real, intent(out) :: A_p
+  real, intent(out) :: R_d                             !day respiration rate               [micro mol/(m^2*s)]
+  real, intent(out) :: O3up
+  real, intent(out) :: fO3_d
+  real, intent(out) :: fO3_h
+
+  ! parameters considered (or defined) to be constant for all species
+  real, parameter :: O_i = 210.0           !O2 concentration                   [mmol/mol]
+  real, parameter :: E_K_C = 79430.0       !activation energy of K_C           [J/mol]            Medlyn2002
+  real, parameter :: E_K_O = 36380.0       !activation energy of K_O           [J/mol]            Medlyn2002
+  real, parameter :: E_R_d = 53000.0       !activation energy of R_d           [J/mol]            Leuning1995
+  real, parameter :: E_Gamma_star = 37830.0 !activation energy for C-comp-point [J/mol]            Medlyn2002
+  real, parameter :: K_C_25 = 404.9        !K.C at reference temperature 25    [micro mol/mol]    Medlyn2002
+  real, parameter :: K_O_25 = 278.4        !K.O at reference temperature 25    [mmol/mol]         Medlyn2002
+  real, parameter :: R_d_20 = 0.32         !R_d at reference temperature 20    [micro mol/(m^2*s)]Leuning1995
+  real, parameter :: Gamma_star_25 = 42.75 !CO2 compensation point at T= 25    [micro mol/mol]    Medlyn2002
+  real, parameter :: A_j_a = 4.0           !electron requirement for NADPH formation
+  real, parameter :: A_j_b = 8.0           !electron requirement for ATP formation
+
+  ! species spedific model parameters (that don't tend to have species specific
+  ! values, others are supplied as arguments)
+  real, parameter :: alpha = 0.3           !efficiency light energy conversion [mol electrons/mol photons]
+  real, parameter :: Teta = 0.90           !shape of J~Q determining factor    []
+  real, parameter :: H_a_jmax = 50300      !activation energy for J_max        [J/mol]
+  real, parameter :: H_d_jmax = 152044     !deactivation energy for J_max      [J/mol]
+  real, parameter :: H_a_vcmax = 73637     !activation energy for V_cmax       [J/mol]
+  real, parameter :: H_d_vcmax = 149252    !deactivation energy for V_cmax     [J/mol]
+  real, parameter :: S_V_vcmax = 486       !entropy terms                      [J/(mol*K)]
+  real, parameter :: S_V_jmax = 495        !entropy terms                      [J/(mol*K)
+  real, parameter :: fDO3 = 0.93
+  real, parameter :: Gamma_3 = 0.5
+
+  ! state variables
+                            !triose phosphate utilisation lim. ass. rate   [micro mol/(m^2*s)]
+  real :: O3up_acc
+  real :: fO3_l
+  real :: f_LA
+  real :: f_LS
+  real :: Gamma_star                      !CO2 comp. point without day resp.  [micro mol/mol]
+  real :: K_C                             !Michaelis constant CO2             [micro mol/mol]
+  real :: K_O                             !Michaelis constant O2              [mmol/mol]
+  real :: J                               !Rate of electron transport         [micro mol/(m^2*s)]
+  real :: Gamma                           !CO2 compensation point             [micro mol/mol]
+  real :: V_cmax                          !Max catalytic rate of Rubisco      [micro mol/(m^2*s)]
+  real :: e_sat_i                         !internal saturation vapour pressure[Pa]
+  real :: h_s                             !relative humidity at leaf surface  [decimal fraction]
+  real :: h_s_VPD                         !VPD at leaf surface                [Pa]
+  real :: c_s                             !CO2 concentration at leaf surface  [micromol/mol]
+  real :: c_i                             !CO2 concentration inside stomata   [micromol/mol]
+  real :: t_l
+  real :: t_lem
+  real :: t_lma
+  real :: t_lep
+  real :: t_lse
+  real :: rO3_s
+  real :: td_dd
+
+  ! iteration parameters
+  integer :: iterations                   !number of the iterations bofore convergence
+  real :: c_i_sup                         !CO2 concentration inside stomata possible through supply
+  integer :: k                            !loop parameters
+
+  c_i = c_i_start
+  g_sto = g_sto_0
+
+  do k=1,50
+
+      O3up = O3 * g_sto * fDO3
+
+      fO3_h = calc_fO3_h(O3up)
+
+      f_LA = calc_f_LA(t_lem, t_lma, t_l, td_dd)
+
+      if (dd == 1) then
+      rO3_s = f_LA
+      else
+      rO3_s = fO3_d_hist(dd-1) + (1 - fO3_d_hist(dd-1)) * f_LA
+      end if
+
+      if (dd == 1) then
+      fO3_d = rO3_s
+      else
+      fO3_d = fO3_h_1_hist(dd) * rO3_s
+      end if
+
+      O3up_acc = O3up_acc_out + O3up
+
+      fO3_l = 1 - (Gamma_3 * O3up_acc_out) ! USING YESTERDAY'S VALUE, COULD CHANGE TO ABOVE.
+
+      t_lma = (t_lep + t_lse) * fO3_l
+
+      t_lse = 0.33 * t_lma
+
+      f_LS = calc_f_LS(t_lem, t_lep, t_lma, t_l, fO3_l, td_dd)
+
+      ! Rubisco activity limited assimilation rate
+      A_c = V_cmax * ((c_i - Gamma_star) &
+      / (c_i + (K_C * (1 + (O_i / K_O))))) * fO3_d * f_LS
+
+      ! RuBP regeneration (electron transport) limited assimilation rate
+      A_j = J * ((c_i - Gamma_star) &
+      / ((A_j_a * c_i) + (A_j_b * Gamma_star)))
+
+      ! Triose phosphate utilisation limited assimilation rate
+      A_p = 0.5 * V_cmax
+
+      ! CO2 assimilation rate
+      A_n = min(A_c, A_j, A_p) - R_d
+
+      ! Surface CO2
+      c_s = c_a - (A_n * (1.37/g_bl))
+
+      ! Surface humidity
+      h_s = (g_sto*e_sat_i + g_bl*e_a) / (e_sat_i * (g_sto + g_bl))
+      ! Convert relative humidity to VPD
+      h_s_VPD = e_sat_i - (e_sat_i * h_s)
+
+      ! Stomatal conductance
+      ! TODO: use humidity deficit version instead
+      g_sto = g_sto_0 + m * (A_n / ((1 + (h_s_VPD/D_0)) * (c_s - Gamma)))*1e6
+
+      ! CO2 supply
+      c_i_sup = c_a - ((A_n*(1.6/g_sto + 1.37/g_bl))*1e6)
+
+      !exits the loop when c_i calculated with both ways meet the convergence
+      !criterium:
+
+      iterations = k
+      if (abs(c_i - c_i_sup) < 0.001) then
+      exit
+      end if
+
+      !Guesses a new c_i as the mean of the first guess and c_i resulting from
+      !the supply function:
+
+      c_i      = c_i-(c_i-c_i_sup)/2
+
+  end do
+
+  end subroutine get_co2_concentration_in_stomata
+
 
         !> Model the net assimilation rate of C3 plants according to the model
         !! developed by Farquar (1980).  Outputs are stomatal conductance and net
@@ -205,10 +373,10 @@ module DO3SE_Photosynthesis_far_ml
             real, intent(in) :: V_cmax_25     !< Maximum catalytic rate at 25 degrees (umol/m^2/s)
             real, intent(in) :: J_max_25      !< Maximum rate of electron transport at 25 degrees (umol/m^2/s)
             real, intent(in) :: D_0           !< "The VPD at which g_sto is reduced by a factor of 2" (Pa) (Leuning et al. 1998)
-            real, intent(in) :: O3
-            real, intent(in) :: td
-            integer, intent(in) :: dd
-            integer, intent(in) :: hr
+            real, intent(in) :: O3            !< ??
+            real, intent(in) :: td            !< ??
+            integer, intent(in) :: dd         !< Day number
+            integer, intent(in) :: hr         !< Hour number
 
             real, dimension(366), intent(inout) :: fO3_h_1_hist
             real, dimension(366), intent(inout) :: fO3_d_hist
@@ -293,25 +461,29 @@ module DO3SE_Photosynthesis_far_ml
             integer :: k                            !loop parameters
 
             if (hr == 0) then
-            daily_thermal_temperatures(dd) = td
+                daily_thermal_temperatures(dd) = td  !< Set the start temperature for that day
             end if
+
 
             if (dd >= season%Astart) then
-            td_dd = td - daily_thermal_temperatures(season%Astart)
+                td_dd = td - daily_thermal_temperatures(season%Astart) !< During the growing season we get the difference between the temperature ...
             else
-            td_dd = 0
+                td_dd = 0 !< before growing season td_dd is 0
             end if
 
+            ! Extract photosynthesis params
             t_l = pgc%t_l
             t_lem = pgc%t_lem
             t_lma = pgc%t_lma
             t_lep = pgc%t_lep
             t_lse = pgc%t_lse
 
+            ! get Leaf temperature in Kelvin
             Tleaf_K = T0 + Tleaf_C
 
             ! Calculation of the model variables which are only
             ! dependend on environmental conditions:
+            ! Using Photosynthesis helper functions
 
             Gamma_star = temp_dep(Gamma_star_25, T0 + 25, E_Gamma_star, Tleaf_K, R)
 
@@ -333,10 +505,16 @@ module DO3SE_Photosynthesis_far_ml
             Gamma        = (Gamma_star+(K_C*R_d*(1+(O_i/K_O))/V_cmax))/&
             (1-(R_d/V_cmax))
 
-            !The following loop guesses a start value for c_i and tests whether
-            !it satisfies all the relevant restrictions. If not a new value for
+            !The following loop guesses a start value for c_i(CO2 concentration inside stomata)
+            !and tests whether it satisfies all the relevant restrictions. If not a new value for
             !c_i is tested:
 
+            !=================
+            ! call get_co2_concentration_in_stomata(0.0, c_a, e_a, g_bl, g_sto_0, m, D_0, O3, &
+            ! dd, O3up_acc_out, & !<< Inputs
+            ! fO3_h_1_hist, fO3_d_hist, & !<< Inouts
+            ! g_sto, A_n, A_c, A_j, A_p, R_d, O3up, fO3_d, fO3_h) ! << Oututs
+            !=====================
             c_i         = 0.0
 
             ! gsto needs a starting point, so let's set it to g_sto_0
@@ -344,75 +522,76 @@ module DO3SE_Photosynthesis_far_ml
 
             do k=1,50
 
-            O3up = O3 * g_sto * fDO3
+              O3up = O3 * g_sto * fDO3
 
-            fO3_h = calc_fO3_h(O3up)
+              fO3_h = calc_fO3_h(O3up)
 
-            f_LA = calc_f_LA(t_lem, t_lma, t_l, td_dd)
+              f_LA = calc_f_LA(t_lem, t_lma, t_l, td_dd)
 
-            if (dd == 1) then
-            rO3_s = f_LA
-            else
-            rO3_s = fO3_d_hist(dd-1) + (1 - fO3_d_hist(dd-1)) * f_LA
-            end if
+              if (dd == 1) then
+              rO3_s = f_LA
+              else
+              rO3_s = fO3_d_hist(dd-1) + (1 - fO3_d_hist(dd-1)) * f_LA
+              end if
 
-            if (dd == 1) then
-            fO3_d = rO3_s
-            else
-            fO3_d = fO3_h_1_hist(dd) * rO3_s
-            end if
+              if (dd == 1) then
+              fO3_d = rO3_s
+              else
+              fO3_d = fO3_h_1_hist(dd) * rO3_s
+              end if
 
-            O3up_acc = O3up_acc_out + O3up
+              O3up_acc = O3up_acc_out + O3up
 
-            fO3_l = 1 - (Gamma_3 * O3up_acc_out) ! USING YESTERDAY'S VALUE, COULD CHANGE TO ABOVE.
+              fO3_l = 1 - (Gamma_3 * O3up_acc_out) ! USING YESTERDAY'S VALUE, COULD CHANGE TO ABOVE.
 
-            t_lma = (t_lep + t_lse) * fO3_l
+              t_lma = (t_lep + t_lse) * fO3_l
 
-            t_lse = 0.33 * t_lma
+              t_lse = 0.33 * t_lma
 
-            f_LS = calc_f_LS(t_lem, t_lep, t_lma, t_l, fO3_l, td_dd)
+              f_LS = calc_f_LS(t_lem, t_lep, t_lma, t_l, fO3_l, td_dd)
 
-            ! Rubisco activity limited assimilation rate
-            A_c = V_cmax * ((c_i - Gamma_star) &
-            / (c_i + (K_C * (1 + (O_i / K_O))))) * fO3_d * f_LS
+              ! Rubisco activity limited assimilation rate
+              A_c = V_cmax * ((c_i - Gamma_star) &
+              / (c_i + (K_C * (1 + (O_i / K_O))))) * fO3_d * f_LS
 
-            ! RuBP regeneration (electron transport) limited assimilation rate
-            A_j = J * ((c_i - Gamma_star) &
-            / ((A_j_a * c_i) + (A_j_b * Gamma_star)))
+              ! RuBP regeneration (electron transport) limited assimilation rate
+              A_j = J * ((c_i - Gamma_star) &
+              / ((A_j_a * c_i) + (A_j_b * Gamma_star)))
 
-            ! Triose phosphate utilisation limited assimilation rate
-            A_p = 0.5 * V_cmax
+              ! Triose phosphate utilisation limited assimilation rate
+              A_p = 0.5 * V_cmax
 
-            ! CO2 assimilation rate
-            A_n = min(A_c, A_j, A_p) - R_d
+              ! CO2 assimilation rate
+              A_n = min(A_c, A_j, A_p) - R_d
 
-            ! Surface CO2
-            c_s = c_a - (A_n * (1.37/g_bl))
+              ! Surface CO2
+              c_s = c_a - (A_n * (1.37/g_bl))
 
-            ! Surface humidity
-            h_s = (g_sto*e_sat_i + g_bl*e_a) / (e_sat_i * (g_sto + g_bl))
-            ! Convert relative humidity to VPD
-            h_s_VPD = e_sat_i - (e_sat_i * h_s)
+              ! Surface humidity
+              h_s = (g_sto*e_sat_i + g_bl*e_a) / (e_sat_i * (g_sto + g_bl))
+              ! Convert relative humidity t150
 
-            ! Stomatal conductance
-            ! TODO: use humidity deficit version instead
-            g_sto = g_sto_0 + m * (A_n / ((1 + (h_s_VPD/D_0)) * (c_s - Gamma)))*1e6
+              h_s_VPD = e_sat_i - (e_sat_i * h_s)
 
-            ! CO2 supply
-            c_i_sup = c_a - ((A_n*(1.6/g_sto + 1.37/g_bl))*1e6)
+              ! Stomatal conductance
+              ! TODO: use humidity deficit version instead
+              g_sto = g_sto_0 + m * (A_n / ((1 + (h_s_VPD/D_0)) * (c_s - Gamma)))*1e6
 
-            !exits the loop when c_i calculated with both ways meet the convergence
-            !criterium:
+              ! CO2 supply
+              c_i_sup = c_a - ((A_n*(1.6/g_sto + 1.37/g_bl))*1e6)
 
-            iterations = k
-            if (abs(c_i - c_i_sup) < 0.001) then
-            exit
-            end if
+              !exits the loop when c_i calculated with both ways meet the convergence
+              !criterium:
 
-            !Guesses a new c_i as the mean of the first guess and c_i resulting from
-            !the supply function:
+              iterations = k
+              if (abs(c_i - c_i_sup) < 0.001) then
+              exit
+              end if
 
-            c_i      = c_i-(c_i-c_i_sup)/2
+              !Guesses a new c_i as the mean of the first guess and c_i resulting from
+              !the supply function:
+
+              c_i      = c_i-(c_i-c_i_sup)/2
 
             end do
 
